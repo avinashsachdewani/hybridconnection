23940d244d23c4799cdf3d7292fe590e65ff07bd
avinashsachdewani <avinash.sachdewani@motifworks.com>
2016-08-30 14:38:54 +0530

    changes committed.

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-var sql = require('mssql');
+//var sql = require('mssql');
 var express = require('express');
-var config = {
-    user: 'sa',
-    password: 'user11',
-    server: 'TESTVM\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance
-    database: 'MWSInstanceSitecore_Master'
-}
+//var sql = require('sqlserver');
 var app = express();
 app.set('port', process.env.PORT || 3000);
-app.get('/getData', function (req, res) {
-    sql.connect(config, function (err) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        var request = new sql.Request();
-        request.query('SELECT * FROM Archive', function (err, recordset) {
-            res.send(recordset);
-        });
-    });   
+app.get('/getData', function (request, res) {
+    var onPremConnString = request.service.config.appSettings.onPremisesDatabase;
+    console.log(onPremConnString);
+    return response.send(onPremConnString);
 });
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
 module.exports = app;
