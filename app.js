191e520c8b05dc79d880cdc2c94ec332c49917ea
avinashsachdewani <avinash.sachdewani@motifworks.com>
2016-08-31 13:27:39 +0530

    changes committed.

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 //var sql = require('mssql');
 var express = require('express');
-//var sql = require('sqlserver');
+var sql = require('mssql');
+var config = {
+    user: 'sa',
+    password: 'user11',
+    server: 'TESTVM', // You can use 'localhost\\instance' to connect to named instance
+    database: 'OnPremisesDB'
+}
 var app = express();
 app.set('port', process.env.PORT || 3000);
-app.get('/getData', function (request, res) {
-    var onPremConnString = request.service.config.appSettings.onPremisesDatabase;
-    console.log(onPremConnString);
-    return response.send(onPremConnString);
+app.get('/', function (req, res) {
+    sql.connect(config, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var request = new sql.Request();
+        request.query('SELECT * FROM AspNetUsers', function (err, recordset) {
+            res.send(recordset);
+        });
+    });
 });
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
